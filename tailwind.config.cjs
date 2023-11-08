312991bb7b4b11407ed2a979162265619fe01fa3
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-11-08 16:25:31 +0700

    build: add tailwindcss/typography plugins

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.export = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
